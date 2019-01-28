246953fe033f6275c4cc68b7b27170909aeec80d
rowenalim95 <rowenalimyiyin@gmail.com>
2019-01-28 23:49:30 +0800

    added index

---
+++ precache-manifest.b6ced6e1378d123528e7c5eb75207a89.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ce1d06f6bc269b2e95f1bf225a54933",
+    "url": "/static/media/education.5ce1d06f.svg"
+  },
+  {
+    "revision": "be581138ec3305dbe1c3",
+    "url": "/static/css/main.217c5d17.chunk.css"
+  },
+  {
+    "revision": "55d5a787af84a71c39d3d6769955b62a",
+    "url": "/static/media/about-background.55d5a787.svg"
+  },
+  {
+    "revision": "7be1595cfd9bd66ce61d",
+    "url": "/static/js/1.7be1595c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ee42085d916e3e955f29cead96a8c7a",
+    "url": "/static/media/work.8ee42085.svg"
+  },
+  {
+    "revision": "be581138ec3305dbe1c3",
+    "url": "/static/js/main.be581138.chunk.js"
+  },
+  {
+    "revision": "30b060612b52fef99c340034be3d0e16",
+    "url": "/static/media/logo_NUS.30b06061.png"
+  },
+  {
+    "revision": "2e88fceab611d934db66294c8bd67a7a",
+    "url": "/static/media/logo_NYP.2e88fcea.png"
+  },
+  {
+    "revision": "c7e38e6f4217092fc65b60e60b4ec91b",
+    "url": "/static/media/about-background-mobile.c7e38e6f.svg"
+  },
+  {
+    "revision": "7be1595cfd9bd66ce61d",
+    "url": "/static/css/1.81ead588.chunk.css"
+  },
+  {
+    "revision": "4de659187dd45e86aa3506a1d051f291",
+    "url": "/index.html"
+  }
+];
